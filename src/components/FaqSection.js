c1921c21ec0e77fbf3022ff3f025c042b33ac371
Shoozle <sean@seanmurtagh.uk>
2021-11-05 13:24:15 +0000

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,37 @@
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div className="Answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, enim.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Well, how did I get here?</h4>
+                <div className="Answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, enim.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What happened to my beautiful wife?</h4>
+                <div className="Answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, enim.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What happened to my beautiful house?</h4>
+                <div className="Answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, enim.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection
