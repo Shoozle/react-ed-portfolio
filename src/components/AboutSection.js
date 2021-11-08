b775d654f61a03dd72b5b1de56f7bc6179627e4e
Shoozle <sean@seanmurtagh.uk>
2021-11-08 00:01:29 +0000

    old motion framer, new one bugs out

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,22 +1,23 @@
 import home1 from '../../src/img/Man.jpg'
-
 import { Hide, Description, About, Image } from '../styles';
+import { motion } from 'framer-motion';
 
 const AboutSection = () => {
+
     return (
         <About>
             <Description>
-                <div className="title">
+                <motion.div  className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true</h2>
+                        <motion.h2>true</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Contact us for any photography or videography ideas that you have. We have amazing staff with amazing skills</p>
                 <button>Contact Us</button>
             </Description>
