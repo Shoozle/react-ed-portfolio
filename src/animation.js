009085cfd4ddda0be7f728ee58d4af1862892634
Shoozle <sean@seanmurtagh.uk>
2021-11-08 01:20:26 +0000

    Animated about us section

--- src/animation.js
+++ src/animation.js
@@ -7,14 +7,42 @@ export const pageAnimation = {
         opacity: 1,
         y: 0,
         transition: {
-            duration: 1
+            duration: 1,
+            when: "beforeChildren",
+            staggerChildren: .25
         }
     },
     exit: {
         opacity: 0,
         y: 300,
         transition: {
-            duration: 1
+            duration: .5
         }
     }
+}
+
+export const titleAnimation = {
+    hidden: {
+        y:200
+
+    },
+    show: {
+        y:0,
+        transition: {duration: .75, ease: "easeOut"}
+    }
+}
+
+export const fade = {
+    hidden: {
+        opacity: 0
+    },
+    show: {
+        opacity: 1,
+        transition: {ease: "easeOut", duration: .75}
+    }
+}
+
+export const photoAnim = {
+    hidden: {scale: 1.5, opacity: 0},
+    show: {scale: 1, opacity: 1, transition: {ease: "easeOut", duration: .75}}
 }
